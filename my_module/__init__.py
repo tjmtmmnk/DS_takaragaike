4ba5e3046b19b527b70364de0ef07ddfe6a45255
TsujimotoTakumi <tsujimoto027@gmail.com>
2018-10-14 14:39:42 +0900

    style: ファイル名を変更

--- my_module/__init__.py
+++ my_module/__init__.py
@@ -1,3 +1,5 @@
-from config import TakaragaikeConfig
+from takaragaike_config import TakaragaikeConfig
 from control_browser import ControlBrowser
 from scraping import Scraping
+from data_base import DataBase
+from data_base_config import DataBaseConfig
